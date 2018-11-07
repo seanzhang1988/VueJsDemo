5482a0e351cc7a9ac913436bf054e00d45fe762b
sean <seanzhang1988@sina.cn>
2018-11-07 17:40:29 +0800

    提交day2

--- node-server/day2/app.js
+++ node-server/day2/app.js
@@ -6,26 +6,26 @@ const urlModule = require('url') //这个核心模块能帮我们解析URL地址
 const server = http.createServer()
 
 //监听http服务器的request请求
-server.on('request', function(req, res){
+server.on('request', function (req, res) {
     // const url = req.url
-    const { pathname:url, query } =urlModule.parse(req.url,true)
+    const { pathname: url, query } = urlModule.parse(req.url, true)
 
-    if(url === '/getscript'){
+    if (url === '/getscript') {
         //拼接一个合法的js脚本，这里拼接的是一个方法的调用,发送给客户端
         // var scriptStr = 'showScipt()'
 
         var data = {
-            name:'zx',
-            age:18,
+            name: 'zx',
+            age: 18,
         }
         var scriptStr = `${query.callback}(${JSON.stringify(data)})`//json对象不能直接调用toString()方法
         res.end(scriptStr)
-    }else{
+    } else {
         res.end('404')
     }
 })
 
-server.listen(3000, function(){
+server.listen(3000, function () {
     console.log('server listen at localhost:3000')
 })
 //启动服务器 nodemon app ,需要npm install -g nodemon 
@@ -36,4 +36,4 @@ server.listen(3000, function(){
   2.新建终端
   3.进入app.js（拿本例来说）所在目录：F:\MyCode\GitRepository\VueJsDemo\node-server\day2
   4.输入命令：nodemon app 即可开启服务
-  */
+*/
